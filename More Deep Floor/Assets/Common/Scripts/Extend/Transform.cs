5ba068c2d23b2be48f267d9328691f944c5183e0
JioBani <spdlqj8876@gmail.com>
2023-06-22 00:46:10 +0900

    Transform 확장함수 추가

---
+++ More Deep Floor/Assets/Common/Scripts/Extend/Transform.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExtensionMethods
+{
+    public static class TransformExtensions
+    {
+        public static void EachChild(this Transform t , Action<Transform> check)
+        {
+            for (int i = 0; i < t.childCount; i++)
+            {
+                check(t.GetChild(i));
+            }
+        }
+    }
+}
