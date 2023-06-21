6d78d9d649c6efe8f194e65f878a102a4f9d7e87
JioBani <spdlqj8876@gmail.com>
2023-06-21 14:05:47 +0900

    Array Save Get 확장함수 추가

---
+++ More Deep Floor/Assets/Common/Scripts/Extend/Array.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using Logger = LNK.MoreDeepFloor.Common.Loggers.Logger;
+using Object = System.Object;
+
+namespace ExtensionMethods
+{
+    public static class ArrayExtensions
+    {
+        public static T SaveGet<T>(this T[] array , int index , T defaultValue)
+        {
+            try
+            {
+                if (array.Length <= index)
+                    throw new IndexOutOfRangeException();
+                else
+                    return array[index];
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e);
+                Logger.LogWarning($"[Array.SaveGet()] 배열 참조 오류 : [{index}]의 값이 ${defaultValue}로 리턴됨");
+                return defaultValue;
+            }
+        }
+    }
+}
