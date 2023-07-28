c05a31997f8e47ad96e04bed157c5ddeac742429
JioBani <spdlqj8876@gmail.com>
2023-07-28 22:12:04 +0900

    EntityManager 제작 시작

---
+++ More Deep Floor/Assets/Scenes/InGame/Scripts/EntityManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using LNK.MoreDeepFloor.InGame.Entitys;
+using UnityEngine;
+
+namespace LNK.MoreDeepFloor.InGame
+{
+    public class EntityManager : MonoBehaviour
+    {
+        //#. 참조
+        //[SerializeField] private DefenderManager defenderManager;
+        //[SerializeField] private MonsterManager monsterManager;
+
+        //#. 변수
+        private List<Defender> battleDefenders;
+        private List<Monster> battleMonsters;
+
+    }
+}
+
+
